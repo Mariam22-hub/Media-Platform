d23d03a341c86e39512a2083f42ea67490859f2a
Mariam22-hub <mariam.elmoazen@gmail.com>
2024-04-23 00:17:04 +0200

    commit

--- index.ts
+++ index.ts
@@ -29,8 +29,8 @@ app.use(require("./src/middlewares/errorHandler"));
 app.use(express.json({ limit: "10000mb" }));
 app.use(express.urlencoded({ limit: "10000mb", extended: true }));
 
-app.options("*", cors({ origin: ['http://localhost:5173', "https://minly-task-jc4q.onrender.com"], optionsSuccessStatus: 200 }));
-app.use(cors({origin: ["http://localhost:5173", "https://minly-task-jc4q.onrender.com"], credentials: true, optionsSuccessStatus: 200 }))
+app.options("*", cors({ origin: ['http://localhost:5173', "https://minly-task-jc4q.onrender.com", "*"], optionsSuccessStatus: 200 }));
+app.use(cors({origin: ["http://localhost:5173", "https://minly-task-jc4q.onrender.com", "*"], credentials: true, optionsSuccessStatus: 200 }))
 
 app.get('/events', (req: express.Request, res: express.Response) => {
     res.setHeader('Content-Type', 'text/event-stream');
